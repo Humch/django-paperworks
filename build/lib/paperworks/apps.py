3a758cee4a173015ae58744917638ee8392ba1bc
Fabien Schlegel <fabienschlegel@yahoo.fr>
2016-12-28 10:42:25 +0100

    update gitignore

--- build/lib/paperworks/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaperworksConfig(AppConfig):
-    name = 'paperworks'
-
-    def ready(self):
-        import paperworks.signals
-        import paperworks.urls
